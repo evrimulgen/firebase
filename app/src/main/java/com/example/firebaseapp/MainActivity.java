d8d1fad6038d02819cf744c47a4ecb42c40f0ada
sourcecode121 <sourcecode121@users.noreply.github.com>
2016-11-27 03:37:51 +0530

    Adding storage, notifications, remote config

--- app/src/main/java/com/example/firebaseapp/MainActivity.java
+++ app/src/main/java/com/example/firebaseapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.firebaseapp;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
@@ -9,6 +10,7 @@ import android.support.v7.widget.RecyclerView;
 import android.text.Editable;
 import android.text.InputFilter;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -20,6 +22,8 @@ import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.ChildEventListener;
@@ -27,16 +31,26 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity implements OnItemClickListener {
 
+    private static final String TAG = MainActivity.class.getSimpleName();
     private static final int MAX = 1000;
+    private static final String MSG_LENGTH_KEY = "msg_max_length";
     private static final String DEFAULT_USERNAME = "Anonymous";
     private static final int RC_SIGN_IN = 1;
+    private static final int RC_PHOTO_PICKER = 5;
 
     private String userName;
     private List<ChatMessage> chatMessages;
@@ -54,6 +68,9 @@ public class MainActivity extends AppCompatActivity implements OnItemClickListen
     private ChildEventListener childEventListener;
     private FirebaseAuth firebaseAuth;
     private FirebaseAuth.AuthStateListener authStateListener;
+    private FirebaseStorage firebaseStorage;
+    private StorageReference photosStorageReference;
+    private FirebaseRemoteConfig firebaseRemoteConfig;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -63,8 +80,11 @@ public class MainActivity extends AppCompatActivity implements OnItemClickListen
         // Initialize firebase objects
         firebaseDatabase = FirebaseDatabase.getInstance();
         firebaseAuth = FirebaseAuth.getInstance();
+        firebaseStorage = FirebaseStorage.getInstance();
+        firebaseRemoteConfig = FirebaseRemoteConfig.getInstance();
 
         messagesDatabaseReference = firebaseDatabase.getReference().child("messages");
+        photosStorageReference = firebaseStorage.getReference().child("chat_photos");
 
         photoPickerButton = (ImageButton) findViewById(R.id.photoPickerButton);
         messageEditText = (EditText) findViewById(R.id.messageEditText);
@@ -86,6 +106,10 @@ public class MainActivity extends AppCompatActivity implements OnItemClickListen
         photoPickerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/jpeg");
+                intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
+                startActivityForResult(Intent.createChooser(intent, "Complete action using"), RC_PHOTO_PICKER);
             }
         });
 
@@ -141,6 +165,45 @@ public class MainActivity extends AppCompatActivity implements OnItemClickListen
                 }
             }
         };
+
+        FirebaseRemoteConfigSettings remoteConfigSettings = new FirebaseRemoteConfigSettings.Builder()
+                                                                .setDeveloperModeEnabled(BuildConfig.DEBUG)
+                                                                .build();
+        firebaseRemoteConfig.setConfigSettings(remoteConfigSettings);
+
+        Map<String, Object> configMap = new HashMap<>();
+        configMap.put(MSG_LENGTH_KEY, MAX);
+        firebaseRemoteConfig.setDefaults(configMap);
+
+        fetchConfig();
+    }
+
+    private void fetchConfig() {
+        long cacheExpiration = 3600;
+        if (firebaseRemoteConfig.getInfo().getConfigSettings().isDeveloperModeEnabled()) {
+            cacheExpiration = 0;
+        }
+        firebaseRemoteConfig.fetch(cacheExpiration)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        firebaseRemoteConfig.activateFetched();
+                        applyRetrievedConfig();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error fetching config", e);
+                        applyRetrievedConfig();
+                    }
+                });
+    }
+
+    private void applyRetrievedConfig() {
+        Long msg_max_length = firebaseRemoteConfig.getLong(MSG_LENGTH_KEY);
+        messageEditText.setFilters(new InputFilter[]{new InputFilter.LengthFilter(msg_max_length.intValue())});
+        Log.i(TAG, MSG_LENGTH_KEY + " = " + msg_max_length);
     }
 
     private void onSignedInInitialize(String displayName) {
@@ -220,6 +283,21 @@ public class MainActivity extends AppCompatActivity implements OnItemClickListen
                 Toast.makeText(this, "Sign in canceled", Toast.LENGTH_SHORT).show();
                 finish();
             }
+        }
+        else if (requestCode == RC_PHOTO_PICKER && resultCode == RESULT_OK) {
+            Uri imageUri = data.getData();
+            StorageReference photoRef = photosStorageReference.child(imageUri.getLastPathSegment());
+            // Upload file to storage
+            photoRef.putFile(imageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    Uri downloadUrl = taskSnapshot.getDownloadUrl();
+                    if (downloadUrl != null) {
+                        ChatMessage chatMessage = new ChatMessage(userName, null, downloadUrl.toString());
+                        messagesDatabaseReference.push().setValue(chatMessage);
+                    }
+                }
+            });
         }
     }
 
